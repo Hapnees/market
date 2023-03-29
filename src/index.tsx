92e8d584160bdc28cfe2704f51b6d114a449393d
Hapnees <8dwardm8lton@gmail.com>
2023-03-29 03:08:50 +0300

    Feat (index.tsx): used HashRouter instead of BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './store/store'
 import './index.scss'
@@ -13,12 +13,12 @@ import 'react-toastify/dist/ReactToastify.css'
 const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter basename={process.env.PUBLIC_URL}>
+	<HashRouter basename={process.env.PUBLIC_URL}>
 		<ToastContainer {...toastContainerConfig} />
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<App />
 			</PersistGate>
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 )
